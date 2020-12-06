7dd5ceb23c9644842c8d070a433703667ff7b328
Tim Gilboy <timgilboy@tims-mbp.mynet>
2020-12-06 13:02:17 +0000

    Day 6 done, finally

---
+++ advent_of_code_day6.py
@@ -0,0 +1,62 @@
+import numpy as np
+
+input = "advent_of_code_day6_input.txt"
+
+def part_one_customs_check(input_file):
+  input = open(input_file, "r")
+  current_group_unique_answers = []
+  unique_answer_counts = []
+  for row in input:
+    row=row.strip()
+    if len(row) < 1:
+      unique_answer_counts.append(len(current_group_unique_answers))
+      current_group_unique_answers = []
+    else:
+      for character in range(0, len(row), 1):
+        if row[character] != "" or row[character] != "\n":
+          if row[character] not in current_group_unique_answers:
+            current_group_unique_answers.append(row[character])
+  total_unique_answer_sum = np.sum(unique_answer_counts) 
+  return total_unique_answer_sum
+
+print("The total number of unique answers in part one is %s" % part_one_customs_check(input))
+
+def part_two_customs_check(input_file):
+  # sourcery skip: inline-immediately-returned-variable, merge-nested-ifs
+  input = open(input_file, "r")
+  current_group_unanimous_answers = []
+  unique_answer_counts = []
+  group_row_counter = 0
+  for row in input:
+    row=row.strip()
+    if len(row) < 1:
+      unique_answer_counts.append(len(current_group_unanimous_answers))
+      current_group_unanimous_answers = []
+      group_row_counter = 0
+    else:
+      if group_row_counter == 0:
+        for character in range(0, len(row), 1):
+          if row[character] != "" or row[character] != "\n":
+            current_group_unanimous_answers.append(row[character])
+        group_row_counter += 1
+      else:
+       #Defines the answers for an individual in a group who isn't the first to answer
+        current_individual_answers = []
+        for character in range(0, len(row), 1):
+          if row[character] != "" or row[character] != "\n":
+              current_individual_answers.append(row[character])
+        #Need to create a temporary version of the group answers to iterate over
+        answer_checker = 0 
+        while answer_checker < len(current_group_unanimous_answers):
+          answer = current_group_unanimous_answers[answer_checker]
+          if answer not in current_individual_answers:
+            current_group_unanimous_answers.remove(answer)
+          else:
+            answer_checker += 1
+        group_row_counter += 1
+
+  print(unique_answer_counts)
+  total_unique_answer_sum = np.sum(unique_answer_counts) 
+  return total_unique_answer_sum
+
+print("The total number of unique answers in part two is %s" % part_two_customs_check(input))
