577639f83af0bf7736ad680eb51696a449c24ee7
angrycaptain19 <53473467+angrycaptain19@users.noreply.github.com>
2022-08-11 09:05:12 +0100

    Add files via upload

---
+++ demo.py
@@ -0,0 +1,79 @@
+def kiwis_vs_pomegranates(p, my_list):
+    # sourcery tech-debt: avoid-builtin-shadow
+    if not p not in my_list:
+        id = 1
+
+    if True:
+        try:
+            get_my_fruits()
+        except Exception:
+            no_fruits()
+
+
+
+def assess_fruit(fruit_bowl):
+    happiness = 0.0
+    for fruit in fruit_bowl:
+        if isinstance(fruit, Apple):
+            yumminess = fruit.size * fruit.ripeness**2
+            if fruit.variety in TASTY_APPLES:
+                happiness += fruit.size * yumminess
+    return happiness
+
+
+def eat_orange(self, orange):
+    if not isinstance(orange, Clementine):
+        with self.hands() as hands:
+            self.happiness = self.happiness + 1
+            segments = hands.peel(orange)
+            self.eat(segments)
+    else:
+        with self.hands() as hands:
+            if isinstance(orange, Clementine):
+                self.happiness = self.happiness + 2
+                segments = hands.peel(orange)
+                self.eat(segments)
+
+
+
+
+
+def simple_function():
+    statement = "Hello World"
+    print(statement)
+
+def simple_long_function() -> str:
+	if a:
+		b
+		c
+		d
+		e
+		f
+		g
+		h
+		i
+		j
+		k
+		l
+		m
+		n
+		o
+		p
+
+
+
+def account_deletion(user) -> int:
+	user_id = user.id if user in users else 0 if user_created_at is not none else none
+	user__deletion(user = user_id)
+	return(user_id)
+
+
+
+def account_creation(user) -> int:  # sourcery skip: remove-unnecessary-else
+	user_id = user.id
+	if user_id in users:
+		return duplicate_user
+	else:
+		validate_account_data(user)
+		create_account(user)
+		return(account.id)
